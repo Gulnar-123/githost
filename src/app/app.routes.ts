e4b272e9cb879e6671ae96e30118dddc0d95265c
Gulnar-123 <gulnar6816@gmail.com>
2025-04-11 10:00:49 +0530

    third commit31

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,11 @@ import { RegisterComponent } from './register/register.component';
 import { ViewComponent } from './view/view.component';
 
 export const routes: Routes = [
-   
+    {path:'home',component:HomeComponent},
+    {path:'',redirectTo:'home',pathMatch:'full'},
+
+    {path:'register',component:RegisterComponent},
+    {path:'view',component:ViewComponent},
+    {path:'**',redirectTo:'home',pathMatch:'full'},
+
 ];
